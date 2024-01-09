d77e8e59743a5e034352c9bd63931d005c2bc9ff
unknown <jfelipem115@gmail.com>
2024-01-09 18:57:26 -0300

    Começando projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string mensagemDeBoasVindas = "Seja bem vindo ao Screen Sound";
+
+
+void InicioProjeto()
+{
+    Console.WriteLine("\r\n╔═══╗─────────────╔═══╗──────────╔╗\r\n║╔═╗║─────────────║╔═╗║──────────║║\r\n║╚══╦══╦═╦══╦══╦═╗║╚══╦══╦╗╔╦═╗╔═╝║\r\n╚══╗║╔═╣╔╣║═╣║═╣╔╗╬══╗║╔╗║║║║╔╗╣╔╗║\r\n║╚═╝║╚═╣║║║═╣║═╣║║║╚═╝║╚╝║╚╝║║║║╚╝║\r\n╚═══╩══╩╝╚══╩══╩╝╚╩═══╩══╩══╩╝╚╩══╝");
+    Console.WriteLine("**********************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("**********************");
+}
+
+InicioProjeto();
